a2171a105fee2ea0cd2442317a511124810db18e
Ben Vilakazi <116912017+BenVilakazi@users.noreply.github.com>
2023-08-10 13:10:40 -0500

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point of the program
- * 
- * This program prints out a message using the puts() function.
- *
- * Return: Always 0 (success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-} 
+}
