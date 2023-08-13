1fb3739a1349c209bbf181f97b5d5de29259d7c5
Ben Vilakazi <116912017+BenVilakazi@users.noreply.github.com>
2023-08-13 04:51:25 +0200

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,50 +1,45 @@
 #include <stdio.h>
 /**
- * main - main block
- * Description: Print all possible combinations of two two-digit numbers.
- * Numbers should range from 0 to 99.
- * The two numbers should be separated by a space.
- * All numbers should be printed with two digits. 1 should be printed as 01.
- * Combination of numbers must be separated by a comma followed by a space.
- * Combinations of numbers should be printed in ascending order.
- * `00 01` and `01 00` are considered as the same combination.
- * You can only use `putchar` to print to console.
- * You can only use `putchar` up to 8 times.
- * You are not allowed to use any variable of type `char`.
- * Return: 0
+ * main - (Start) returns every combination of 2 & 2 digits
+ * Return: 0 (Success execution)
  */
 int main(void)
 {
-	int i, j;
-	int a, b, c, d;
+	int i, j, k, h;
 
-	for (i = 0; i < 100; i++)
+	i = 0;
+	j = i;
+	while (i < 10)
 	{
-		a = i / 10; /* doubles fnum */
-		b = i % 10; /* singles fnum */
-
-		for (j = 0; j < 100; j++)
+		while (j < 10)
 		{
-			c = j / 10; /* doubles snum */
-			d = j % 10; /* singles snum */
-
-			if (a < c || (a == c && b < d))
+			k = i;
+			h = j + 1;
+			while (k < 10)
 			{
-				putchar(a + '0');
-				putchar(b + '0');
-				putchar(32);
-				putchar(c + '0');
-				putchar(d + '0');
-
-				if (!(a == 9 && b == 8))
+				while (h < 10)
 				{
-					putchar(44);
-					putchar(32);
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(h + '0');
+					if ((i == 9) && (j == 8) && (k == 9) && (h == 9))
+						;
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					h++;
 				}
+				h = 0;
+				k++;
 			}
+			j++;
 		}
+		j = 0;
+		i++;
 	}
-	putchar(10);
-
+	putchar('\n');
 	return (0);
-}
