82f73e5c24191e923f2ed0b4d0dc1204c51d1dfd
Ben Vilakazi <xolanithegeengubane@gmail.com>
2023-08-21 13:38:03 +0200

    C pointers, 4

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - random password generator
+ * Return: 0 indicates successful execution
+ */
+int main(void)
+{
+	int sum = 0;
+	char c;
+
+	srand(time(NULL));
+	while (sum < 2772)
+	{
+		c = rand() % 94 + 33;
+		putchar(c);
+		sum += c;
+	}
+	putchar(2772 - sum);
+	return (0);
+}
+
