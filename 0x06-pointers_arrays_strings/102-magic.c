91fbb32b5442b09e625d39be249a1b5805519d70
Ben Vilakazi <116912017+BenVilakazi@users.noreply.github.com>
2023-08-23 21:44:24 +0200

    Update 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
-
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+  int n;
+  int a[5];
+  int *p;
 
-	a[2] = 1024;
-	p = &n;
-	/*
-	* write your line of code here...
-	* main - this is a desc
-	*/
-	*(p + 5) = 98;
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you're not allowed to use a
+   * - you're not allowed to modify p
+   * - only one statement
+   * - you're not allowed to code anything other than this line of code
+   */
+  *(p + 5) = 98;
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
