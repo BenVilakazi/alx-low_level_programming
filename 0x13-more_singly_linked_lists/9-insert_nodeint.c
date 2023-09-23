8ab13d34812f0fb752dfa081bbe8e2741c3910d2
Ben Vilakazi <116912017+BenVilakazi@users.noreply.github.com>
2023-09-23 21:41:28 +0000

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,61 +1,49 @@
 #include "lists.h"
 /**
- * list_size - funt for len elemnt
- * @h: single list in
- * Return: num of elem in the linked lis
- */
-int list_size(listint_t **h)
-{
-	listint_t *auxiliar;
-	int i = 0;
-
-	auxiliar = *h;
-	while (auxiliar != NULL)
-	{
-		i++;
-		auxiliar = auxiliar->next;
-	}
-	return (i);
-}
-/**
- * insert_nodeint_at_index - insert new node in the index position
- * @head: linked list
- * @idx: index, it start in 0
- * @n: data for new node
- * Return: Always addres new node
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: double pointer to the head of the listint list
+ * @idx: is the index of the list where the new node should be added
+ * @n: is the data
+ * Return: Null if it failed or the address of the new node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-unsigned int iterator = 0;
-unsigned int lenght = list_size(head);
-listint_t *auxiliar = *head;
-listint_t *new_node = NULL;
+	listint_t *headaux;
+	listint_t *newnode;
+	unsigned int size;
 
-if (head == NULL || idx > lenght)
-	return (NULL);
-
-	new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
-	return (NULL);
-
-	new_node->n = n;
+	size = 0;
+	/* allocate memory for the newnode */
+	newnode = malloc(sizeof(listint_t));
+	/* task condition */
+	if (newnode == NULL)
+		return (NULL);
+	/* n keeps being the data of the new node */
+	newnode->n = n;
+	/*  given the case, if index is cero then newnode is equal to head */
+	/* and return newnode in head of the list */
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		newnode->next = *head;
+		*head = newnode;
+		return (newnode);
 	}
-	
-	while (auxiliar != NULL)
+	/* usea de auxiliar */
+	headaux = *head;
+	/* if index is different to the position */
+	/* counter size keeps running and headaux moves to the next node */
+	while (headaux != NULL && size != idx - 1)
 	{
-		if (iterator == (idx - 1))
-		{
-			new_node->next = auxiliar->next;
-			auxiliar->next = new_node;
-			return (new_node);
-		}
-		auxiliar = auxiliar->next;
-		iterator++;
+		size++;
+		headaux = headaux->next;
 	}
+	/* if index and size are equal and non NULL */
+	if (size == idx - 1 && headaux != NULL)
+	{
+		newnode->next = headaux->next;
+		headaux->next = newnode;
+		return (newnode);
+	}
+	free(newnode);
 	return (NULL);
 }
