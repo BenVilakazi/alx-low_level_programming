3a1660298f84d45507f6ab313a4fee038e030110
Ben Vilakazi <116912017+BenVilakazi@users.noreply.github.com>
2023-09-28 10:54:01 +0000

    la

--- 0x14-bit_manipulation/0-binary_to_uint..c
+++ 0x14-bit_manipulation/0-binary_to_uint..c
@@ -1,31 +1,32 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
  * binary_to_uint - binary to unsigned int
- * @b: binary num
+ * @b: binary number
  * Return: unsigned int number
  **/
 unsigned int binary_to_uint(const char *b)
 {
-    int result = 0, i = 0, pow = 1;
+int result = 0, i = 0, pow = 1;
 
-    if (b == NULL)
-    return (0);
+if (b == NULL)
+return (0);
 
-    while (b[i] != '\0')
-    {
-        i++;
-    }
-    i--;
+while (b[i] != '\0')
+{
+i++;
+}
+i--;
 
-    while (i >= 0)
-    {
-        if (b[i] != '0' && b[i] != '1')
-        return (0);
+while (i >= 0)
+{
+if (b[i] != '0' && b[i] != '1')
+return (0);
 
-        result += (b[i] - 48) * pow;
+result += (b[i] - 48) * pow;
 
-        pow *= 2;
-        i--;
-    }
-    return (result);
+pow *= 2;
+i--;
+}
+return (result);
 }
