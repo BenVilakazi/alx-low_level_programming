c3f1e1e288f84c39e197f98b41ca48992dcbd157
Ben Vilakazi <116912017+BenVilakazi@users.noreply.github.com>
2023-09-28 11:04:43 +0000

    nyeke

--- 0x14-bit_manipulation/ 0-binary_to_uint.c
+++ 0x14-bit_manipulation/ 0-binary_to_uint.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * binary_to_uint - binary to unsigned int
@@ -7,26 +7,18 @@
  **/
 unsigned int binary_to_uint(const char *b)
 {
-int result = 0, i = 0, pow = 1;
+	int i;
+	unsigned int brny = 0;
 
-if (b == NULL)
-return (0);
+	if (!b)
+		return (0);
 
-while (b[i] != '\0')
-{
-i++;
-}
-i--;
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		brny = 2 * brny + (b[i] - '0');
+	}
 
-while (i >= 0)
-{
-if (b[i] != '0' && b[i] != '1')
-return (0);
-
-result += (b[i] - 48) * pow;
-
-pow *= 2;
-i--;
-}
-return (result);
+	return (brny);
 }
