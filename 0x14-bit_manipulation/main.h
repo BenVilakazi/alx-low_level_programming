79fe7cb0858afa9bf93ec885098df847bed8e970
Ben Vilakazi <116912017+BenVilakazi@users.noreply.github.com>
2023-09-28 11:23:25 +0000

    last

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,16 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
+/*
+ * File: main.h
+ * File Descp: A Header File  for all Prototypes.
+ * Created: 28th of September, 2023
+ * Auth: Ben Vilakazi
+ * Size: undefined.
+ * Project: alx-low_level_programming.
+ * Status: submitted.
+ */
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
