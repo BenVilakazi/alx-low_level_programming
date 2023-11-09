2982fefacbc9adee5966939d67704328027382b2
Ben Vilakazi <116912017+BenVilakazi@users.noreply.github.com>
2023-11-09 12:32:49 +0200

    Update 103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,37 +1,132 @@
-#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
- * add_dnodeint_end - adds a new node at the end
- * of a dlistint_t list
- * @head: head of the list
- * @n: value of the elem
- * Return: the address of the new elem
+ * f3 - crackme
+ * @i: first variable
+ * @j: second variable
+ * Return: char
  */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+int f3(char *i, int j)
 {
-	dlistint_t *h;
-	dlistint_t *new;
+	int a;
+	int b;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->next = NULL;
-
-	h = *head;
-
-	if (h != NULL)
+	a = 1;
+	b = 0;
+	while (b < j)
 	{
-		while (h->next != NULL)
-			h = h->next;
-		h->next = new;
+		a = i[b] * a;
+		b = b + 1;
 	}
-	else
+	return ((a ^ 0x55) & 0x3f);
+}
+
+/**
+ * f4 - crackme
+ * @i: first variable
+ * @j: second variable
+ * Return: char
+ */
+int f4(char *i, int j)
+{
+	int a;
+	int b;
+
+	a = (int)*i;
+	b = 0;
+	for (; b < j; b++)
 	{
-		*head = new;
+		if (a < (int)i[(long)b])
+		{
+			a = (int)i[(long)b];
+		}
 	}
+	srand(a ^ 0xe);
+	return (rand() & 0x3f);
+}
 
-	new->prev = h;
+/**
+ * f5 - crackme
+ * @i: first variable
+ * @j: second variable
+ * Return: char
+ */
+int f5(char *i, int j)
+{
+	int a;
+	int b;
 
-	return (new);
+	a = 0;
+	b = 0;
+	while (b < j)
+	{
+		a = a + (int)i[(long)b] * (int)i[(long)b];
+		b = b + 1;
+	}
+	return ((a ^ 0xef) & 0x3f);
+}
+
+/**
+ * f6 - crackme
+ * @i: first variable
+ * Return: char
+ */
+int f6(char i)
+{
+	int a;
+	int b;
+
+	a = 0;
+	b = 0;
+	while (b < i)
+	{
+		a = rand();
+		b = b + 1;
+	}
+	return ((a ^ 0xe5) & 0x3f);
+}
+
+/**
+ * main - Entry Point
+ * @argc: count
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int a, b, c;
+	char *s;
+	long int arr[] = {
+		0x3877445248432d41,
+		0x42394530534e6c37,
+		0x4d6e706762695432,
+		0x74767a5835737956,
+		0x2b554c59634a474f,
+		0x71786636576a6d34,
+		0x723161513346655a,
+		0x6b756f494b646850
+	};
+
+	if (argc != 2)
+		exit(1);
+
+	s = argv[1];
+	a = strlen(s);
+	putchar(((char *)arr)[(a ^ 59) & 63]);
+	c = 0;
+	b = 0;
+	while (b < a)
+	{
+		c = c + s[b];
+		b = b + 1;
+	}
+	putchar(((char *)arr)[(c ^ 0x4f) & 0x3f]);
+	putchar(((char *)arr)[f3(s, a)]);
+	putchar(((char *)arr)[f4(s, a)]);
+	putchar(((char *)arr)[f5(s, a)]);
+	putchar(((char *)arr)[f6(*s)]);
+
+	return (0);
 }
