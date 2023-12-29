f886c0b75954b59e16a4afb337db03e40c418618
Ben Vilakazi <116912017+BenVilakazi@users.noreply.github.com>
2023-12-29 19:35:15 +0200

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,11 +16,13 @@ if (new_dog == NULL)
 return (NULL);
 }
 new_dog->name = malloc(strlen(name) + 1);
+  
 if (new_dog->name == NULL)
 {
 free(new_dog);
 return (NULL);
 }
+  
 new_dog->owner = malloc(strlen(owner) + 1);
 if (new_dog->owner == NULL)
 {
@@ -28,6 +30,7 @@ free(new_dog->name);
 free(new_dog);
 return (NULL);
 }
+  
 strcpy(new_dog->name, name);
 strcpy(new_dog->owner, owner);
 new_dog->age = age;
